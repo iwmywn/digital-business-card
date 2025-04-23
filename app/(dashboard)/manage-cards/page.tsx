df2cf930a6c394414c693bc41afb5c79151779bc
Anh Tuan <thongocnguyen1011@gmail.com>
2025-04-23 22:21:20 +0700

    chore: update

--- app/(dashboard)/manage-cards/page.tsx
+++
@@ -1,9 +0,0 @@
-import type { Metadata } from "next";
-
-export function generateMetadata(): Metadata {
-  return { title: "Manage Cards" };
-}
-
-export default function page() {
-  return <div>page</div>;
-}
