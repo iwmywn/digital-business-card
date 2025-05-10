58b7b9c5728063cdf12be5b29eecf92b8a12e5a9
Anh Tuan <thongocnguyen1011@gmail.com>
2025-05-10 16:24:26 +0700

    chore: rm unnecessary api endpoint

--- app/api/(auth)/_signout/route.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { session } from "@/lib/session";
-import { NextResponse } from "next/server";
-
-export async function POST() {
-  await session.user.delete();
-  return new NextResponse(null, { status: 204 });
-}
