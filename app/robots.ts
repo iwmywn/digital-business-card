20d15370b5e3c17eb1c15005bd1fc98ae74c2598
Anh Tuan <thongocnguyen1011@gmail.com>
2025-05-15 22:26:04 +0700

    fix: allow /card/ and /profile/ paths in robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -4,6 +4,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
+      allow: ["/card/", "/profile/"],
       disallow: "/",
     },
     sitemap: `${process.env.NEXT_PUBLIC_URL}/sitemap.xml`,
