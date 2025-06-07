bb48c5fb12d8e3b41246fe420bed6ce76cbea44a
Anh Tuan <thongocnguyen1011@gmail.com>
2025-06-07 17:59:10 +0700

    chore: update Stripe API version

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -4,7 +4,7 @@ import { createResponse } from "@/app/api/utils";
 import { processSuccessfulPayment } from "@/actions/stripe-utils";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-04-30.basil",
+  apiVersion: "2025-05-28.basil",
 });
 
 export async function POST(req: NextRequest) {
