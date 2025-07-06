0ee9b466a3e62464a6a57d88da1f227a44054ba5
Anh Tuan <thongocnguyen1011@gmail.com>
2025-07-06 23:28:39 +0700

    chore: update deps

--- actions/stripe.ts
+++ actions/stripe.ts
@@ -6,7 +6,7 @@ import { getUserById } from "@/lib/data";
 import { processSuccessfulPayment } from "./stripe-utils";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-05-28.basil",
+  apiVersion: "2025-06-30.basil",
 });
 
 export async function createCheckoutSession(priceId: string, planId: string) {
--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -6,7 +6,7 @@ import { createResponse } from "@/app/api/utils";
 import { processSuccessfulPayment } from "@/actions/stripe-utils";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-05-28.basil",
+  apiVersion: "2025-06-30.basil",
 });
 
 export async function POST(req: NextRequest) {
